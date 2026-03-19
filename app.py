84a8d40b9cfe23cbc06ab0b39be3b19ecc35d261
Adrija <adrijagen21@gmail.com>
2026-03-19 23:43:27 +0530

    Prepared for deployment

--- app.py
+++ app.py
@@ -44,4 +44,9 @@ def redirect_url(short):
 
 if __name__ == '__main__':
     init_db()
-    app.run(debug=True)
+    import os
+
+if __name__ == '__main__':
+    init_db()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
